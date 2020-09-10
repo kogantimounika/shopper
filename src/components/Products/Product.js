eb57797d0595c7fc6d81ef020525700f43515f7e
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-09-10 15:18:12 -0700

    Added products

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,39 +1,25 @@
-// import React, { Component } from 'react';
-// export default class Product extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     render() {
-//         return (
-//             <div>
-              
-
-//             </div>
-//         )
-//     }
-// }
-
-
 import React, { Component } from 'react';
+import './Product.css'
 
 class Product extends Component {
     constructor(props) {
         super(props);
-        
     }
     render() {
         const prods=this.props.products;
+        console.log(prods)
         return (
             <div>
-                <ul>
+                <ul className="products">
                     {this.props.products.map((product)=>(
                         <li key={product.id}>
-                            <div>
+                            <div className="image-div">
                             <a href="#">
                                 <img src={require(`./Images/${product.image}`)} 
                                      alt={product.title}></img>
                                 <p>{product.title}</p>
                             </a>
+                            <p>${product.price}</p>
                             </div>
                         </li>
                     ))}
