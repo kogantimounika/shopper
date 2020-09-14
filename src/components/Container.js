341104c12f318cbc1b6d6a0e2cd04d2604994e78
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-09-14 11:59:13 -0700

    added filter and sort functions to the container

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,10 +7,50 @@ import Filter from "./Filter/Filter";
 class Container extends Component {
     constructor(props){
         super(props);
+    
         this.state={
-            products:data.products
+            products:data.products,
+            sort:"",
+            size : ""
         }
     }
+    sortProducts=(event) =>{
+        // alert(event.target.value);
+        const sort = event.target.value;
+        this.setState(
+            {
+                sort:sort,
+                products:data.products.sort((a,b)=>{
+                    alert(a.price);
+                    alert(b.price);
+                    if(sort==="lowest"){
+                        return a.price-b.price
+                    }
+                    else{
+                        return b.price-a.price
+                    }
+                })
+            }
+        )
+
+    }
+    filterProducts = (event) => {
+        if(event.target.value === "") {
+            this.setState ( {
+                size:event.target.value,
+                products:data.products
+            } ) 
+        } else {
+            this.setState ({
+                size : event.target.value,
+                products:data.products.filter ((product) => (
+                    product.availableSizes.indexOf(event.target.value) > 0
+
+                ))
+            })
+        }
+    }
+
     render() {
         return (
             <div className="grid-container">
@@ -19,7 +59,8 @@ class Container extends Component {
                 </div>
                 <div className="main">
                     <div className="mainContent">
-                        <Filter></Filter> 
+                        <Filter sortProducts={this.sortProducts} 
+                        filterProducts = {this.filterProducts}></Filter> 
                         <Product products={this.state.products}/>
                     </div>
                     <div className="sideBar">
