23c54578ba6e47e6d8219597a23597db39cf1108
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-09-15 14:21:44 -0700

    Added count of products

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,64 +1,3 @@
-// import React, { Component } from 'react';
-// import './Product.css'
-
-// class Product extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     render() {
-//         const prods=this.props.products;
-//         console.log(prods)
-//         return (
-//             // <div>
-//             //     <div className = "Content">
-//             //         <div class="dropdown">
-//             //             <button class="dropbtn">Order</button>
-//             //             <div class="dropdown-content">
-//             //                 <a href="/">Latest</a>
-//             //                 <a href="/">Previous</a>
-//             //             </div> 
-//             //         </div>
-//             //         <label for = "filters">Filter </label>
-//             //         <select id = "filter">
-//             //             <option value = "all">ALL</option>
-//             //             <option value = "some">SOME</option>
-//             //         </select> 
-//             // </div>
-//             <div>   
-//                 <ul className="products">
-//                     {this.props.products.map((product)=>(
-//                         <li key={product.id}>
-//                         <div className="image-div">
-//                             <a href="#">
-//                                 <img src={require(`./Images/${product.image}`)} 
-//                                      alt={product.title}></img>
-//                                 <p>{product.title}</p>
-//                             </a>
-//                             <div className = "des">
-//                                 <p>${product.price}</p>
-//                                 {/* <div> */}
-//                                     <button type="button">Add To Cart</button>
-//                                 {/* </div> */}
-//                             </div>
-//                         </div>
-//                         </li>
-//                     ))}
-//                 </ul>
-//             </div>
-//             // </div>
-//         );
-//     }
-// }
-
-// export default Product;
-
-
-
-
-
-
-
-
 import React, { Component } from 'react';
 import './Product.css';
 
@@ -80,7 +19,10 @@ class Product extends Component {
                                 <p>{product.title}</p>
                                 <div className="price">
                                     <p>Amount:{product.price}</p>
-                                    <button>ADD to Cart</button>
+                                    <button onClick={()=>this.props.addToCart(product)}
+                                    >ADD to Cart</button>
+ {/* onClick={()=>this.props.addToCart(prod) */}
+
                                 </div>
                             </a>
                             </div>
