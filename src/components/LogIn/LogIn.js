ce7de366e36b2f91093e65ceac5e332b432bfc7b
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-09-28 22:02:39 -0700

    Added login page to the project

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import history from '../history';
 
 
 class LogIn extends Component {
     constuctor() {
         this.routeChange = this.routeChange.bind(this);
-      }
+    }
     routeChange() {
         let path = `../Container`;
         history.push(path);
-      }
+    }
     render() {
         return (
             <div>
                 LOGIN INFO
-                <input type = "text"></input>
-                <button  onClick={this.routeChange}>SUBMIT</button>
+                <br></br>
+                <input type="text"></input>
+                <br></br>
+                <button onClick={this.routeChange}>SUBMIT</button>
             </div>
         );
     }
