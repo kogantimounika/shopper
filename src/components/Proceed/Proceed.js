272fde255a3ce6f8973a13238bb3e096e2770602
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-09-28 22:04:57 -0700

    Added components to the proceed page

--- src/components/Proceed/Proceed.js
+++ src/components/Proceed/Proceed.js
@@ -5,31 +5,30 @@ import Payment from "../Payment/Payment";
 export default class Proceed extends Component {
     constructor(props) {
         console.log(props);
-        super(props); 
-            this.state = {
-                makePayment : false
-            }   
+        super(props);
+        this.state = {
+            makePayment: false
+        }
     }
-    
+
     render() {
         return (
             <div>
-                <div className = "myClass">
-               <label> Name:
+                <div className="myClass">
+                    <label> Name:
                     <input type="text" name="name" />
-                </label>
-                <label> Email:
+                    </label>
+                    <label> Email:
                     <input type="email" name="email" />
-                </label>
-                <label> PhNo:
+                    </label>
+                    <label> PhNo:
                     <input type="PhNo" name="PhNo" />
-                </label>
+                    </label>
                 </div>
-                
-                <input type="submit" value="Submit" class = "button"  
-                onClick = {() => this.setState({makePayment:true}) } /> 
 
-{this.state.makePayment && <Payment></Payment> }  
+                <input type="submit" value="Submit" className="button"
+                    onClick={() => this.setState({ makePayment: true })} />
+                {this.state.makePayment && <Payment></Payment>}
             </div>
         )
     }
