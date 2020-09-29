a9ae163c16a484402b6fe88709d05bdf89b25925
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-09-28 22:05:22 -0700

    Added new products

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import './Product.css';
 
 class Product extends Component {
-    constructor(props) {
-        super(props);
-        
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
+
     render() {
-        const prods=this.props.products;
+        const prods = this.props.products;
+        console.log(prods);
         return (
             <div>
                 <ul className="products">
-                    {this.props.products.map((product)=>(
+                    {this.props.products.map((product) => (
                         <li key={product.id}>
                             <div>
-                            <a href="#">
-                                <img src={require(`./Images/${product.image}`)} alt={product.title}></img>
-                                <p>{product.title}</p>
-                                <div className="price">
-                                    <p>Amount:{product.price}</p>
-                                    <button onClick={()=>this.props.addToCart(product)}
-                                    >ADD to Cart</button>
-                                </div>
-                            </a>
+                                <a href="#">
+                                    <img src={require(`./Images/${product.image}`)} alt={product.title}></img>
+                                    <p>{product.title}</p>
+                                    <div className="price">
+                                        <p>Amount:{product.price}</p>
+                                        <button onClick={() => this.props.addToCart(product)}
+                                        >ADD to Cart</button>
+                                    </div>
+                                </a>
                             </div>
                         </li>
                     ))}
